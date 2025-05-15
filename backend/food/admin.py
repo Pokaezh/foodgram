f35d56b3a1974a0cbc4b2d5e7bc28bb91151a02c
Nikolai Pokazanev <pokaezh@yandex.ru>
2025-05-15 21:04:42 +0500

    fix_flake8

--- backend/food/admin.py
+++ backend/food/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from food.models import CookUser, Ingredient, Recipe, Tag
+from food.models import Ingredient, Recipe, Tag
 
 User = get_user_model()
 
@@ -20,30 +20,30 @@ class CookUserAdmin(BaseUserAdmin):
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ("name", "measurement_unit")
 
+
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ("name", "slug")
+
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     list_display = (
         "id",
-        "name", 
-        "author", 
-        "image", 
-        "text", 
-        "pub_date", 
-        "get_ingredients", 
-        "get_tags", 
+        "name",
+        "author",
+        "image",
+        "text",
+        "pub_date",
+        "get_ingredients",
+        "get_tags",
         "cooking_time")
 
     def get_ingredients(self, obj):
-        return ", ".join([ingredient.name for ingredient in obj.ingredients.all()])
+        return ", ".join(
+            [ingredient.name for ingredient in obj.ingredients.all()])
     get_ingredients.short_description = 'Ingredients'
 
     def get_tags(self, obj):
         return ", ".join([tag.name for tag in obj.tags.all()])
     get_tags.short_description = 'Tags'
-
-
-       
