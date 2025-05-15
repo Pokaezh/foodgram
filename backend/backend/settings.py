9ecff078e6eb637f34fa4a2e76ea39639758325d
Nikolai Pokazanev <pokaezh@yandex.ru>
2025-05-15 22:55:26 +0500

    fix_pep8

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000",]
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000", ]
 
 CSRF_TRUSTED_ORIGINS = [
     'https://eda-dada.ru',
